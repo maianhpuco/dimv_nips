9384aa8adb53949ceaf38057545ac50223b4030f
Mai Anh Vu <maianh@Mais-MacBook-Pro.local>
2023-05-12 13:09:10 +0700

    adding classification

--- exp/mono/grid_searching.py
+++ exp/mono/grid_searching.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("")
 import numpy as np
 from src.utils import get_directory
-from src.clf import classification, grid_search 
+from src.clf import classification, grid_search, train_test_splitting  
 from src.load_data import load_data 
 import argparse
 import json
@@ -48,28 +48,31 @@ def grid_searching(
         exp_num=None, 
         create_new_exp=False):
 
-    X, y = load_data(ds_name)
-    print(X.shape, y.shape)
+    Xtrain, Xtest, ytrain, ytest, train_indices, test_indices = \
+            train_test_splitting(ds_name)
+    
+    print("grid search shape {} and test set shape {}", Xtrain.shape, Xtest.shape)
     if dryrun == 1:
-        X, y = X[:1000, ], y[:1000]
-        print(X.shape)
-        print(y.shape)
+        Xtrain, ytrain = Xtrain[:1000,], ytrain[:1000]
+        print(Xtrain.shape)
+        print(ytrain.shape)
     
     gtruth_path =  get_save_path(ds_name, exp_num, create_new_exp)
     print("Ground Truth Path: ", gtruth_path) 
 
-    
 
-    #hparams, acc = grid_search(X, y)
-    hparams = {'max_depth': 15, 'learning_rate': 0.1, 'objective': 'multi:softmax', 'num_class': 10, 'eval_metric': 'mlogloss'}
-    acc = 0.9442571428571428
+    hparams, acc = grid_search(Xtrain, ytrain, n_splits = 5)
+
     with open(os.path.join(gtruth_path, "acc_ground_truth.json"), 'w') as f:
         json.dump({"acc": acc}, f) 
 
     with open(os.path.join(gtruth_path, "hyperparameters.json"), 'w') as f:
-        json.dump(hparams, f) 
+        json.dump(hparams, f)
 
+    with open(os.path.join(gtruth_path, "indices.json"), 'w') as f:
+        json.dump({'train_indices': list(train_indices), "test_indices": list(test_indices)}, f)
 
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Imputing data after monotone missing created"
@@ -78,7 +81,7 @@ if __name__ == "__main__":
     parser.add_argument("--ds", type=str, default=None)
     parser.add_argument("--dryrun", type=int, default=0)
     parser.add_argument("--exp_num", type=int, default=None)
-    parser.add_argument("--create_new_exp", type=int, default=None)
+    parser.add_argument("--create_new_exp", type=int, default=False)
 
     args = parser.parse_args()
 
