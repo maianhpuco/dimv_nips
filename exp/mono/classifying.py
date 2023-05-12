f990bdfa370d4fb4f8a7e17be3790d5bb406bdd8
Mai Anh Vu <maianhuel@gmail.com>
2023-05-12 13:09:10 +0700

    adding classification

--- exp/mono/classifying.py
+++ exp/mono/classifying.py
@@ -29,6 +29,7 @@ def get_save_path(ds_name, mrate, exp_num, create_new_exp):
         exp_num =  np.amax(np.array([int(i) for i in integer_files])) + 1
     else: 
         exp_num =  np.amax(np.array([int(i) for i in integer_files]))
+
     _dir = get_directory(
             stage = 'exp', 
             mono_or_rand  = 'mono', 
@@ -51,41 +52,55 @@ def classify(
 
     if dryrun==1:
         missing_rates = [missing_rates[0]] 
-
+        print("this is dryrun mode")
+    
     exp_dir = get_save_path(ds_name, 0, exp_num, create_new_exp)
+    
 
     ground_truth_path = "/".join(exp_dir.split("/")[:-1])
     print("Ground Truth Path: ", ground_truth_path)
     
         
-    acc_f   = open(os.path.join(ground_truth_path, "acc_ground_truth.json"))
-    hparams_f = open(os.path.join(ground_truth_path, "hyperparameters.json"))
-   
+    acc_f        = open(os.path.join(ground_truth_path, "acc_ground_truth.json"))
+    hparams_f    = open(os.path.join(ground_truth_path, "hyperparameters.json"))
+    indices_f =  open(os.path.join(ground_truth_path, "indices.json"))
+        
     hparams = json.load(hparams_f)
     acc_gtruth = json.load(acc_f)
+    indices_dict = json.load(indices_f)
+    train_indices = indices_dict["train_indices"]
+    test_indices  = indices_dict["test_indices"]
     
+    print("train, test indice {} {} ".format(len(train_indices), len(test_indices)))
     print("Best acc ", acc_gtruth) 
     print("Best params ", hparams)
+    
+    X, y = load_data(ds_name)
 
     for mrate in missing_rates:
         save_folder = get_save_path(
             ds_name, mrate, exp_num, create_new_exp)
 
         X_imp_path = os.path.join(save_folder, "X_imp_{}.npz".format(algo))     
-        Ximp = np.load(X_imp_path)["arr_0"] 
-    
-        _, y = load_data(ds_name)          
+
+        Ximp_train = np.load(X_imp_path)["arr_0"][train_indices]
+        y_train  = y[train_indices]
+
+        Xgt_test  = X[test_indices]
+        y_test    = y[test_indices]
 
         if dryrun == 1:
             print("dry runing")
-            Ximp, y = Ximp[:1000], y[:1000]
+            Ximp_train, y_train = Ximp_train[:1000], y_train[:1000]
+            Xgt_test, y_test    = Xgt_test[:200], y_test[:200]
         
-        acc = classification(Ximp, y, hparams)
+        acc = classification(Ximp_train, y_train, Xgt_test, y_test, hparams)
+
         
         with open(os.path.join(save_folder, "acc_{}.json".format(algo)), 'w') as f:
             json.dump({"acc": acc}, f) 
         print(
-                ">> Complete save {} with acc {}  with at path {}"
+                ">> -------Complete save --- {} ----with acc {}  with at path {}"
                 .format(algo, acc, save_folder)
             )
 
